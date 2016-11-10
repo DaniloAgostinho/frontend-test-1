95d6efbb5608a6f03c3412bc86266806b6c03d73
DaniloAgostinho <daniloagostinhosilva@gmail.com>
2016-11-10 15:44:35 -0200

    inclusão formulario desafio 2

--- form cadastro/js/main.js
+++ form cadastro/js/main.js
@@ -1,26 +1,86 @@
-(function (win, doc) {
-  'use strict';
+(function(win, doc) {
+    'use strict';
+    //campos
+    var $nome = doc.getElementById('nome');
+    var $email = doc.getElementById('email');
+    var $telefone = doc.getElementById('telefone');
+    //botoes do formulario
+    var $salvar = doc.getElementById('salvar');
+    var $cancelar = doc.getElementById('cancelar');
 
-   var $nome = doc.getElementById('nome');
-   var $email = doc.getElementById('email');
-   var $telefone = doc.getElementById('telefone');
+    //tr's da tabela
+    var $insertName = doc.querySelector('.insertName');
+    var $insertEmail = doc.querySelector('.insertEmail');
+    var $insertTelefone = doc.querySelector('.insertTelefone');
+
+    //events
+    //$nome.addEventListener('input', callback);
+    //$email.addEventListener('input', callback);
+    //$telefone.addEventListener('input', callback);
+    //botao
+    $salvar.addEventListener('click', callback);
 
 
-   $nome.addEventListener('input', callback);
-   $email.addEventListener('input', callback);
-   $telefone.addEventListener('keypress', validaForm);
+
+    function callback() {
+        var resultName = $nome.value.trim();
+        var resultEmail = $email.value.trim();
+        var resultTelefone = $telefone.value.trim();
 
 
-   function callback() {
-     var resultName = $nome.value.trim();
-     var resultEmail = $email.value.trim();
-     var resultTelefone = $telefone.value.trim();
-   }
+        //$cancelar.addEventListener('input', callback);
 
-   function validaForm(e) {
-     var expressao = /[0-9]/;
-     console.log(expressao);
-   }
+        function criaObjetoNomes() {
+            var informacoes = {
+                "name": resultName,
+                "telefone": resultEmail,
+                "email": resultTelefone
+            };
+
+            console.log(informacoes);
+
+            $insertName.append(informacoes.name);
+            $insertEmail.append(informacoes.telefone);
+            $insertTelefone.append(informacoes.email);
+
+        }
+
+        criaObjetoNomes();
+
+
+        function requestFormulario() {
+            var informacoes = {
+                "name": resultName,
+                "telefone": resultEmail,
+                "email": resultTelefone
+            };
+
+            var xhr = new XMLHttpRequest();
+            xhr.open('GET', 'http://localhost:3000/js/main.js', true);
+            xhr.send();
+
+            xhr.addEventListener('readystatechange', rest);
+            var response = JSON.parse(xhr.responseText);
+
+            function rest() {
+                if (xhr.readyState === 4 && xhr.status === 200) {
+                    var $insertName = doc.querySelector('.insertName');
+                    var $insertEmail = doc.querySelector('.insertEmail');
+                    var $insertTelefone = doc.querySelector('.insertTelefone');
+
+                    $insertName.append(informacoes.name);
+                    $insertEmail.append(informacoes.telefone);
+                    $insertTelefone.append(informacoes.email);
+                }
+            }
+        }
+
+        requestFormulario();
+
+
+
+    }
+
 
 
 })(window, document);
