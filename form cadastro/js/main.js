df0ac7c94b8048ecdeaf9f301797fcf30f53a276
DaniloAgostinho <daniloagostinhosilva@gmail.com>
2016-11-10 23:32:49 -0200

    remocao objeto

--- form cadastro/js/main.js
+++ form cadastro/js/main.js
@@ -5,29 +5,6 @@
     var $email = doc.getElementById('email');
     var $telefone = doc.getElementById('telefone');
 
-    function mascara(o,f){
-    v_obj=o
-    v_fun=f
-    setTimeout("execmascara()",1)
-    }
-    function execmascara(){
-    v_obj.value=v_fun(v_obj.value)
-    }
-    function soLetras(v){
-    return v.replace(/\d/g,"") //Remove tudo o que não é Letra
-    }
-    function soLetrasMA(v){
-    v=v.toUpperCase() //Maiúsculas
-    return v.replace(/\d/g,"") //Remove tudo o que não é Letra ->maiusculas
-    }
-    function soLetrasMI(v){
-    v=v.toLowerCase() //Minusculas
-    return v.replace(/\d/g,"") //Remove tudo o que não é Letra ->minusculas
-    }
-    function soNumeros(v){
-    return v.replace(/\D/g,"") //Remove tudo o que não é dígito
-    }
-
     //botoes do formulario
     var $salvar = doc.getElementById('salvar');
     var $cancelar = doc.getElementById('cancelar');
