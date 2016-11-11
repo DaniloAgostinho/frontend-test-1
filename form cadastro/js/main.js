604fb1300b5e0ef967dce708646a2981c74e543f
DaniloAgostinho <daniloagostinhosilva@gmail.com>
2016-11-10 22:15:38 -0200

    inclusao formulario de cadastro

--- form cadastro/js/main.js
+++ form cadastro/js/main.js
@@ -4,6 +4,35 @@
     var $nome = doc.getElementById('nome');
     var $email = doc.getElementById('email');
     var $telefone = doc.getElementById('telefone');
+
+    function mascara(o, f) {
+        v_obj = o
+        v_fun = f
+        setTimeout("execmascara()", 1)
+    }
+
+    function execmascara() {
+        v_obj.value = v_fun(v_obj.value)
+    }
+
+    function soLetras(v) {
+        return v.replace(/\d/g, "") //Remove tudo o que não é Letra
+    }
+
+    function soLetrasMA(v) {
+        v = v.toUpperCase() //Maiúsculas
+        return v.replace(/\d/g, "") //Remove tudo o que não é Letra ->maiusculas
+    }
+
+    function soLetrasMI(v) {
+        v = v.toLowerCase() //Minusculas
+        return v.replace(/\d/g, "") //Remove tudo o que não é Letra ->minusculas
+    }
+
+    function soNumeros(v) {
+        return v.replace(/\D/g, "") //Remove tudo o que não é dígito
+    }
+
     //botoes do formulario
     var $salvar = doc.getElementById('salvar');
     var $cancelar = doc.getElementById('cancelar');
@@ -76,6 +105,34 @@
             }
         }
     }
+
+    //validação
+    $("#validacao").validate({
+        rules: {
+            nome: {
+                required: true,
+                minlength: 3
+            },
+            telefone: {
+                required: true
+            },
+            email: {
+                required: true
+            }
+        },
+        messages: {
+            nome: {
+                required: "Por favor, informe seu nome!",
+                minlength: "O nome deve ter pelo menos 3 caracteres"
+            },
+            telefone: {
+                required: "Este aceita apenas digitos"
+            },
+            email: {
+                required: "Informe um email válido: tente usar o @"
+            }
+        }
+    });
 
 
 
