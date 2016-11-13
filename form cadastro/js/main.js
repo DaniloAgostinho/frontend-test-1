b5368eff4821ae1ddfcf403aa8bd3ca182dee5ac
DaniloAgostinho <daniloagostinhosilva@gmail.com>
2016-11-13 21:05:17 -0200

    formulario completo

--- form cadastro/js/main.js
+++ form cadastro/js/main.js
@@ -5,42 +5,48 @@
     $salvar = doc.getElementById('salvar');
 
 
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', '/', true);
-  xhr.send();
+
 
 
     $salvar.addEventListener('click', callback);
 
     function callback( event ) {
       //event.preventDefault();
+
         function adicionaContatosLocalStorage() {
+          var xhr = new XMLHttpRequest();
+          xhr.open('GET', '/', true);
+          xhr.send();
+
             var filtroNome = $nome.value.trim();
             var filtroTelefone = $telefone.value.trim();
             var filtroEmail = $email.value.trim();
 
-            //cria o array com os dados Digitados pelo usuário
-            var dadosDigitados = {
-                nome: filtroNome,
-                telefone: filtroTelefone,
-                email: filtroEmail
-            };
-            //converte array para String
-            var stringDados = JSON.stringify(dadosDigitados);
-            //adicionar o array de Dados criado ao localStorage
-            localStorage.setItem("cadastro", stringDados);
-            //recupera os dados do localStorage
-            var dadosRecuperadosLocalStorage = localStorage.getItem("cadastro");
-            //parsea os Dados para o formato JSON
-            var parseaDadosRecuperados = JSON.parse(dadosRecuperadosLocalStorage);
-            //mostra os dados recuperados
-            var nomeRecuperado = parseaDadosRecuperados['nome'];
-            var telefoneRecuperado = parseaDadosRecuperados['telefone'];
-            var emailRecuperado = parseaDadosRecuperados['email'];
+            xhr.addEventListener('readystatechange', function() {
+              //cria o array com os dados Digitados pelo usuário
+              var dadosDigitados = {
+                  nome: filtroNome,
+                  telefone: filtroTelefone,
+                  email: filtroEmail
+              };
+              //converte array para String
+              var stringDados = JSON.stringify(dadosDigitados);
+              //adicionar o array de Dados criado ao localStorage
+              localStorage.setItem("cadastro", stringDados);
+              //recupera os dados do localStorage
+              var dadosRecuperadosLocalStorage = localStorage.getItem("cadastro");
+              //parsea os Dados para o formato JSON
+              var parseaDadosRecuperados = JSON.parse(dadosRecuperadosLocalStorage);
+              //mostra os dados recuperados
+              var nomeRecuperado = parseaDadosRecuperados['nome'];
+              var telefoneRecuperado = parseaDadosRecuperados['telefone'];
+              var emailRecuperado = parseaDadosRecuperados['email'];
 
-            doc.getElementById('showNome').innerHTML = nomeRecuperado;
-            doc.getElementById('showTelefone').innerHTML = telefoneRecuperado;
-            doc.getElementById('showEmail').innerHTML = emailRecuperado;
+              doc.getElementById('showNome').innerHTML = nomeRecuperado;
+              doc.getElementById('showTelefone').innerHTML = telefoneRecuperado;
+              doc.getElementById('showEmail').innerHTML = emailRecuperado;
+            }, false);
+
         };//add contanto
 
         adicionaContatosLocalStorage();
