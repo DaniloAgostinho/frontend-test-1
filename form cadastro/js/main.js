13ddb210e895d6edcc1f7fbe1beb1610032e9bf9
DaniloAgostinho <daniloagostinhosilva@gmail.com>
2016-11-13 17:49:38 -0200

    form de cadastro com localstorage

--- form cadastro/js/main.js
+++ form cadastro/js/main.js
@@ -1,94 +1,58 @@
 (function(win, doc) {
-    'use strict';
-    //campos
-    var $nome = doc.getElementById('nome');
-    var $email = doc.getElementById('email');
-    var $telefone = doc.getElementById('telefone');
-    var $inputs = doc.querySelectorAll('input');
+    $nome = doc.getElementById('nome');
+    $telefone = doc.getElementById('telefone');
+    $email = doc.getElementById('email');
+    $salvar = doc.getElementById('salvar');
 
 
-    //botoes do formulario
-    var $salvar = doc.getElementById('salvar');
-    var $cancelar = doc.getElementById('cancelar');
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', '/', true);
+  xhr.send();
 
-    //tr's da tabela
-    var $insertName = doc.querySelector('.insertName');
-    var $insertEmail = doc.querySelector('.insertEmail');
-    var $insertTelefone = doc.querySelector('.insertTelefone');
 
-    //events
-    //$nome.addEventListener('input', callback);
-    //$email.addEventListener('input', callback);
-    //$telefone.addEventListener('input', callback);
-    //botao
     $salvar.addEventListener('click', callback);
-    $salvar.addEventListener('click', requestFormulario);
-    $cancelar.addEventListener('click', resetFormulario);
 
+    function callback( event ) {
+      //event.preventDefault();
+        function adicionaContatosLocalStorage() {
+            var filtroNome = $nome.value.trim();
+            var filtroTelefone = $telefone.value.trim();
+            var filtroEmail = $email.value.trim();
 
-
-    function callback() {
-        var resultName = $nome.value.trim();
-        var resultEmail = $email.value.trim();
-        var resultTelefone = $telefone.value.trim();
-
-
-        //$cancelar.addEventListener('input', callback);
-
-        function criaObjetoNomes() {
-            var informacoes = {
-                "name": resultName,
-                "telefone": resultEmail,
-                "email": resultTelefone
+            //cria o array com os dados Digitados pelo usuário
+            var dadosDigitados = {
+                nome: filtroNome,
+                telefone: filtroTelefone,
+                email: filtroEmail
             };
+            //converte array para String
+            var stringDados = JSON.stringify(dadosDigitados);
+            //adicionar o array de Dados criado ao localStorage
+            localStorage.setItem("cadastro", stringDados);
+            //recupera os dados do localStorage
+            var dadosRecuperadosLocalStorage = localStorage.getItem("cadastro");
+            //parsea os Dados para o formato JSON
+            var parseaDadosRecuperados = JSON.parse(dadosRecuperadosLocalStorage);
+            //mostra os dados recuperados
+            var nomeRecuperado = parseaDadosRecuperados['nome'];
+            var telefoneRecuperado = parseaDadosRecuperados['telefone'];
+            var emailRecuperado = parseaDadosRecuperados['email'];
 
-            console.log(informacoes);
+            doc.getElementById('showNome').innerHTML = nomeRecuperado;
+            doc.getElementById('showTelefone').innerHTML = telefoneRecuperado;
+            doc.getElementById('showEmail').innerHTML = emailRecuperado;
+        };//add contanto
 
-            $insertName.append(informacoes.name);
-            $insertEmail.append(informacoes.telefone);
-            $insertTelefone.append(informacoes.email);
-
-        }
-
-        criaObjetoNomes();
-
+        adicionaContatosLocalStorage();
     }
 
-    function requestFormulario() {
-        var informacoes = {
-            "name": resultName,
-            "telefone": resultEmail,
-            "email": resultTelefone
-        };
 
-        // Old compatibility code, no longer needed.
-        if (window.XMLHttpRequest) { // Mozilla, Safari, IE7+ ...
-            xhr = new XMLHttpRequest();
-        } else if (window.ActiveXObject) { // IE 6 and older
-            xhr = new ActiveXObject("Microsoft.XMLHTTP");
-        }
-        xhr.open('GET', '/', true);
-        xhr.send();
 
-        xhr.addEventListener('readystatechange', rest);
-        var response = JSON.parse(xhr.responseText);
+    //$nome.addEventListener('input', filtro);
+    //$telefone.addEventListener('input', filtro);
+    //$email.addEventListener('input', filtro);
 
-        function rest() {
-            if (xhr.readyState === 4 && xhr.status === 200) {
-                var $insertName = doc.querySelector('.insertName');
-                var $insertEmail = doc.querySelector('.insertEmail');
-                var $insertTelefone = doc.querySelector('.insertTelefone');
 
-                $insertName.append(informacoes.name);
-                $insertEmail.append(informacoes.telefone);
-                $insertTelefone.append(informacoes.email);
-            }
-        }
-    }
-
-    function resetFormulario() {
-      $inputs.value = '';
-    }
 
 
 
